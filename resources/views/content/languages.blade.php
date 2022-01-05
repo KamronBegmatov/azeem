29860737bf923a4b0fa90cc636963a86dbb2e0bf
malika <malikajuraeva98@gmail.com>
2022-01-05 17:56:17 +0500

    dividing blades into sections

---
+++ resources/views/content/languages.blade.php
@@ -0,0 +1,44 @@
+@extends('layout.default')
+
+@section('content')
+    <div class="row m-2">
+        <div class="col-12">
+            <div class="card">
+
+                <div class="card-body table-responsive p-0" style="height: 300px;">
+                    <table class="table table-head-fixed text-nowrap">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>name</th>
+                                <th>active</th>
+                                <th>iso-code</th>
+                                <th>language-code</th>
+                                <th>action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>Uzbek</td>
+                                <td>1</td>
+                                <td>uz</td>
+                                <td>uz-uz</td>
+                                <td><button class="btn"><i class="fa fa-eye"></i></button></td>
+                            </tr>
+                            <tr>
+                                <td>2</td>
+                                <td>Русский</td>
+                                <td>1</td>
+                                <td>ru</td>
+                                <td>ru-ru</td>
+                                <td><button class="btn"><i class="fa fa-eye"></i></button></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+@stop
--- routes/web.php
+++ routes/web.php
@@ -20,41 +20,8 @@ Route::get('/login', function () {
     return view('login');
 });
 Route::get('/dashboard', function () {
-    return view('dashboard');
-});
-Route::get('/widgets', function () {
-    return view('widgets');
-});
-Route::get('/forms/advanced', function () {
-    return view('forms/advanced');
-});
-Route::get('/forms/editors', function () {
-    return view('forms/editors');
-});
-Route::get('/forms/general', function () {
-    return view('forms/general');
-});
-Route::get('/forms/validation', function () {
-    return view('forms/validation');
-});
-Route::get('/tables/simple', function () {
-    return view('tables/simple');
-});
-Route::get('/tables/data', function () {
-    return view('tables/data');
-});
-Route::get('/tables/jsgrid', function () {
-    return view('tables/simple');
-})->name("jsgrid");
-Route::get('/mailbox/mailbox', function () {
-    return view('/mailbox/mailbox');
-});
-Route::get('/mailbox/compose', function () {
-    return view('/mailbox/compose');
-});
-Route::get('/mailbox/read-mail', function () {
-    return view('/mailbox/read-mail');
+    return view('layout/default');
 });
 Route::get('/languages', function () {
-    return view('/mailbox/read-mail');
+    return view('/content/languages');
 });
